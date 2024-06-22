f89c14f5cd5e23c7fc3df1b20bdc26bed83c9807
MOHIT-SOLANKII <mohitslnki773@gmail.com>
2024-06-22 11:54:27 +0530

    Add login and admin registration functionality

--- controller/userController.js
+++ controller/userController.js
@@ -1,6 +1,7 @@
 import { catchAsyncErrors } from "./../middlewares/catchAsyncErrors.js";
 import ErrorHandler from "./../middlewares/errorMiddleware.js";
 import { User } from "../models/userSchema.js";
+import {generateToken} from "../utils/jwtToken.js"
 
 export const patientRegister = catchAsyncErrors(async (req, res, next) => {
   // Capturing the input filled by user
@@ -50,10 +51,81 @@ export const patientRegister = catchAsyncErrors(async (req, res, next) => {
     password,
     role,
   });
-  res.status(200).json({
+   /* res.status(200).json({
     success: true,
     message: "User registered successfully.",
-  });
+  });*/
+
+  generateToken(user, "User registered successfully.", 200, res);
 });
 
+export const login = catchAsyncErrors(async (req, res, next) => {
+  const { email, password, confirmPassword, role } = req.body;
+  if (!email || !password || !confirmPassword || !role) {
+    return next(new ErrorHandler("Please fill all the fields.", 400));
+  }
+  if (password !== confirmPassword) {
+    return next(
+      new ErrorHandler("Password and Confirm Password do not match.", 400)
+    );
+  }
+  const user = await User.findOne({ email }).select("+password");
+  if (!user) {
+    return next(new ErrorHandler("Invalid Credentials.", 400));
+  }
+  const isPasswordMatched = user.comparePassword(password);
+  if (!isPasswordMatched) {
+    return next(new ErrorHandler("Invalid Credentials.", 400));
+  }
+  if (role !== user.role) {
+    return next(new ErrorHandler("User with given role not found.", 400));
+  }
+
+  /* res.status(200).json({
+    success: true,
+    message: "User Logged In successfully.",
+  });*/
+
+  generateToken(user, "User logged in successfully.", 200, res);
+});
+
+
+export const addNewAdmin = catchAsyncErrors(async (req, res, next) => {
+  const { firstName, lastName, email, phone, nic, dob, gender, password } =
+    req.body;
+  if (
+    !firstName ||
+    !lastName ||
+    !email ||
+    !phone ||
+    !nic ||
+    !dob ||
+    !gender ||
+    !password
+  ) {
+    return next(new ErrorHandler("Please fill all the fields.", 400));
+  }
+
+  const isRegistered = await User.findOne({ email });
+  if (isRegistered) {
+    return next(new ErrorHandler("Admin With This Email Already Exists!", 400));
+  }
+
+  const admin = await User.create({
+    firstName,
+    lastName,
+    email,
+    phone,
+    nic,
+    dob,
+    gender,
+    password,
+    role: "Admin",
+  });
+  res.status(200).json({
+    success: true,
+    message: "New Admin Registered",
+    admin,
+  });
+});
 
